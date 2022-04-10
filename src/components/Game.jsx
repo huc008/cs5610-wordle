b2bc09ca9f1f42ce644111173c7337ebe32711b1
Yanling <huang.yanl@northeastern.edu>
2022-04-09 22:43:26 -0700

    added save data feature

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -14,6 +14,7 @@ export const GameContext = createContext();
 
 export default function Game({row, col, difficultyLevel}) {
     let choosenWordBank = difficultyLevel === "Easy" ? wordBankEasy : difficultyLevel === "Medium" ? wordBankMedium : wordBankHard;
+    let wordLevelLength = difficultyLevel === "Easy" ? 5 : difficultyLevel === "Medium" ? 6 : 7;
     const [openModal, setOpenModal] = useState(false);
     const [board, setBoard] = useState(difficultyLevel === "Easy" ? boardDefaultEasy : difficultyLevel === "Medium" ? boardDefaultMedium : boardDefaultHard);
     const [currTryout, setCurrTryout] = useState({ x_val: 0, y_val: 0 });
@@ -28,14 +29,59 @@ export default function Game({row, col, difficultyLevel}) {
     const handleClose = () => setOpenModal(false);
     const handleShow = () => setOpenModal(true);
 
+    //to save answer when reload with unfinished game:
     useEffect(() => {
-        generateWordsSet(choosenWordBank).then((words) =>{
-            setWordSet(words.wordSet);
-            setCorrectWord(words.selectedMagicWord);
-        });
+        if(window.localStorage.getItem('correctWord') == null || JSON.parse(window.localStorage.getItem('correctWord')).length != wordLevelLength) {
+            console.log("don't have storage correctWord");
+            generateWordsSet(choosenWordBank).then((words) =>{
+                setWordSet(words.wordSet);
+                setCorrectWord(words.selectedMagicWord);
+                window.localStorage.setItem('correctWord', JSON.stringify(words.selectedMagicWord));
+                // console.log("wordSet " + words.wordSet);
+                console.log("Answer_selectedMagicWord: " + words.selectedMagicWord);
+                console.log("Answer_correctWord: " + correctWord);
+            });
+        } else {
+            console.log("find the word ");
+            setCorrectWord(JSON.parse(window.localStorage.getItem('correctWord')));
+            generateWordsSet(choosenWordBank).then((words) =>{
+                setWordSet(words.wordSet);
+            });
+            console.log("Answer_correctWord: " + JSON.parse(window.localStorage.getItem('correctWord')));
+        }
+}, []);
+    
+    //save board data:
+    useEffect(() => {
+        const newboard = JSON.parse(window.localStorage.getItem('board'));
+        if (newboard && choosenWordBank == wordBankEasy && newboard.length === 7) {
+            setBoard(newboard)
+        } else if (newboard && choosenWordBank == wordBankMedium && newboard.length === 6) {
+            setBoard(newboard)
+        } else if (newboard && choosenWordBank == wordBankHard && newboard.length === 5) {
+            setBoard(newboard)
+        }
+        let tmpTryout = JSON.parse(window.localStorage.getItem('currTryout'));
+        if(tmpTryout && !(tmpTryout.x_val == 0 && tmpTryout.y_val == 0)) {
+            tmpTryout = {...tmpTryout, y_val: tmpTryout.y_val + 1};
+            setCurrTryout(tmpTryout); 
+        }
     }, []);
 
-    // console.log(correctWord);
+    useEffect(() => {
+        window.localStorage.setItem('board', JSON.stringify(board));
+        window.localStorage.setItem('currTryout', JSON.stringify(currTryout));
+    }, [board]);
+
+    useEffect(() => {
+        if(gameOver.gameOver){
+            let defaultBoard = difficultyLevel === "Easy" ? boardDefaultEasy : difficultyLevel === "Medium" ? boardDefaultMedium : boardDefaultHard;
+            window.localStorage.setItem('board', JSON.stringify(defaultBoard));
+            window.localStorage.removeItem('correctWord');
+            window.localStorage.setItem('currTryout', JSON.stringify({ x_val: 0, y_val: 0 }));
+        }
+    }, [gameOver.gameOver]);
+
     const onSelectLetter = (keyVal) => {
         if (currTryout.y_val >= col) return;
         const newBoard = [...board];
